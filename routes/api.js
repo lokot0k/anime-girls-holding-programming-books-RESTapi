ae3df64095a9cfabfbea440d938256e92ed2610b
lokot0k <kaptoxa322@mail.ru>
2022-03-10 23:08:50 +0300

    completed GET method for certain language

--- routes/api.js
+++ routes/api.js
@@ -2,19 +2,32 @@ const express = require('express');
 const path = require('path');
 const fs = require("fs");
 const router = express.Router();
+require("dotenv").config();
 
-/* GET pictures by language */
+/* GET all pictures with certain language */
 router.get('/:language', (req, res) => {
-
     let language = req.params.language;
-    if (fs.existsSync(path.join(__dirname, '..', 'public', 'images', language))) {
-        res.json({"success": true});
+    const pathToLanguage = path.join(__dirname, '..', 'public', 'images', language);
+    if (fs.existsSync(pathToLanguage)) {
+        res.status(200);
+        res.json(buildArrayOfContent(pathToLanguage, language));
         res.status(200);
     } else {
-        res.json({"success": false});
         res.status(404);
+        res.json();
     }
 });
+
+function buildArrayOfContent(dirPath, language) {
+    let responseArray = [];
+    const namesArray = fs.readdirSync(dirPath, "utf8");
+    namesArray.forEach(name => responseArray.push({
+        language,
+        name,
+        "size": fs.statSync(path.join(dirPath, name)).size
+    }));
+    return responseArray;
+}
 
 module.exports = router;
 
